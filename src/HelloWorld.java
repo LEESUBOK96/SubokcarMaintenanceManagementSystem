325a890559c3eca135a14ad1081e8b6329c1729a
user <user@DESKTOP-0T3ULGR>
2020-03-18 23:25:00 +0900

    creat menu 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,12 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-		System.out.println("I am a new Java programmer!");
+		System.out.println("+++ Subokcar Maintenance Management System +++");
+		System.out.println(" 1. Add Maintenance");
+		System.out.println(" 2. Delete Maintenance");
+		System.out.println(" 3. Edit Maintenance");
+		System.out.println(" 4. View Maintenance");
+		System.out.println(" 5. Exit");
+		System.out.println(" Select one number between 1-5 :");
 	}
 
 }
