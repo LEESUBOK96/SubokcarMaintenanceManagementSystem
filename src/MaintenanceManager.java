7360d15cb1752b62f8e1e403ba96d69c22cbc98b
user <user@DESKTOP-0T3ULGR>
2020-04-07 12:43:19 +0900

    modified edit menu

--- src/MaintenanceManager.java
+++ src/MaintenanceManager.java
@@ -42,7 +42,44 @@ public class MaintenanceManager {
 			System.out.print("Maintenance Name:");
 			String maintenanceName = input.next();
 			if(maintenance.maintenanceName.equals(maintenanceName)) {
-				System.out.println("the maintenance to be edited is " + maintenanceName);
+				int num = -1;
+				
+				while(num !=5) {
+
+				System.out.println("++ Subokcar Maintenance Info Edit Menu ++");
+				System.out.println(" 1. Edit Maintenance Name");
+				System.out.println(" 2. Edit Maintenance Year");
+				System.out.println(" 3. Edit Maintenance Distancedriven");
+				System.out.println(" 4. Edit Maintenance workshop");
+				System.out.println(" 5. Exit");
+				System.out.print(" Select one number between 1-5 :");
+				num = input.nextInt();
+				if (num == 1) {
+					System.out.print("Maintenance Name:");
+					maintenance.maintenanceName = input.next();	
+					
+				}
+				else if (num == 2) {
+					System.out.print("Maintenance Year:");
+					maintenance.maintenanceYear = input.nextInt();
+					
+				}
+				else if (num == 3) {
+					System.out.print("Distance driven:");
+					maintenance.distancedriven  = input.nextInt();
+					
+					
+				}
+				else if (num == 4) {
+					System.out.print("Workshop:");
+					maintenance.workshop  = input.next();
+					
+					
+				}
+				else {
+					continue;
+				}
+				}
 				
 			}
 			
